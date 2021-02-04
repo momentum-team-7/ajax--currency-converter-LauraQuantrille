b88b9940988650f1032666ca94f31c3dfb090602
LauraQuantrille <lquantrille@gmail.com>
2021-02-03 20:00:11 -0500

    html & attempted js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// const catFacts = document.querySelector('.cat-facts')
+
+// fetch(catUrl)
+// .then (response => response.json())
+// .then (data => {
+//   console.log(data)
+//   const catFact = data[0].text
+//   document.querySelector('.cat-facts').innerHTML = `<p>${catFact}</p>`
+  
+// }
+
+const fromCurrencyEl = document.querySelector('.convert-from')
+const fromAmountEl = document.querySelector('.from-amount-field')
+const toCurrencyEl = document.querySelector('.convert-to')
+const toAmountEl = document.querySelector('.to-amount-field')
+const finalAmountEl = document.querySelector('.final-amount')
+
+
+
+
+fetch ('https://api.exchangeratesapi.io/latest')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    const fromCurrencyEl = data[0].base
+    console.log(fromCurrencyEl)
+  })
+  
+
 const currencies = [
   'EUR',
   'CAD',
